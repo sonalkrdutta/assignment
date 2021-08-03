9b8f2e8e45192a460626e5a0bb6a14c92e61d2d6
sonalkrdutta <sonaldutta3@gmail.com>
2021-08-03 12:34:59 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
 function App() {
+  const [Rheight, setHeight] = useState('');
+  const [Rwidth, setWidth] = useState('');
+  const [bgColor, setBgcolor] = useState('');
+  const [button, setButton] = useState('press');
+  const change = () => {
+    setHeight("50vh")
+    setWidth("50vh")
+    setBgcolor("green")
+    setButton("Button Clicked")
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="position" style={{}}>
+        <div className="rectangle" style={{ backgroundColor: bgColor, height: Rheight, width: Rwidth }} />
+      </div>
+      <h6>Hello</h6>
+      <h6>Hello Hello</h6>
+      <h6>Hello Hello Hello</h6>
+      <h6>Hello Hello Hello Hello</h6>
+      <div className="button-position">
+        <button className="button" onClick={change}>{button}</button>
+      </div>
     </div>
   );
 }
